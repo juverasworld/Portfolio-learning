41690cf4c124745b47f6c5c93e1661be20e1a28f
Vee <ifebuchejulia@gmail.com>
2024-02-04 22:12:14 +0100

    starter apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-
-
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from "./components";
+import Stars from "./components/canvas/Stars";
+const App = () => {
   return (
-    <>
-      <div className="">
-        3D Developer Portfolio
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover  bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <Stars />
+        </div>
       </div>
-    </>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
